26ff5112f323b637dbbea697d632f84f61750667
rejzoo <rejzho@gmail.com>
2025-02-25 11:14:30 +0100

    BE project init

---
+++ hassediagrams-be/src/main/java/com/hassediagrams/rejzo/HassediagramsBeApplication.java
@@ -0,0 +1,13 @@
+package com.hassediagrams.rejzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HassediagramsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HassediagramsBeApplication.class, args);
+	}
+
+}
