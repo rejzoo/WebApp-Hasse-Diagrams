906cde3cdd8877ed9673f00c956400670084f48c
rejzoo <rejzho@gmail.com>
2025-02-28 12:43:08 +0100

    Added clear button and numbering

--- hassediagrams-fe/components/UI/InputTable.tsx
+++ hassediagrams-fe/components/UI/InputTable.tsx
@@ -82,50 +82,68 @@ export default function InputTable({ numberOfElements }: TableProps) {
     console.log(rows);
   }
 
-  return (
-    <><div className="custom-scrollbar overflow-x-auto max-h-[70vh] bg-[var(--itemsbackground)]/15 rounded-2xl shadow-md shadow-gray-800 pb-4">
-      <table className="min-w-full border-collapse">
-        <thead className="sticky top-0 z-20 bg-[var(--itemsbackground)]">
-          <tr>
-            {headers.map((header, index) => (
-              <th
-                key={index}
-                className="h-12 px-4 text-center text-xl font-light uppercase tracking-wider whitespace-nowrap"
-              >
-                {header}
-              </th>
-            ))}
-            <th className="h-12 px-4 text-center text-xl font-light uppercase tracking-wider whitespace-nowrap">
-              System
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((row, rowIndex) => (
-            <tr
-              key={rowIndex}
-              className="hover:bg-[var(--itemsbackground)]/10 transition-colors"
-            >
-              {row.elements.map((value, colIndex) => (
-                <td key={colIndex} className="px-4 py-2 text-center">
-                  <ToggleCell
-                    currentValue={value}
-                    onSelect={(val) => handleElementClick(rowIndex, colIndex, val)} />
-                </td>
-              ))}
-              <td className="px-4 py-2 text-center">
-                <ToggleCell
-                  currentValue={row.system}
-                  onSelect={(val) => handleSystemClick(rowIndex, val)} />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+  const clearValues = () => {
+    setRows(calculateRows(numberOfElements));
+  }
 
-      </div><div className="flex items-center justify-center rounded-md transition-colors text-lg pt-5">
+  return (
+    <>
+      <div className="custom-scrollbar overflow-x-auto max-h-[70vh] bg-[var(--itemsbackground)]/15 rounded-2xl shadow-md shadow-gray-800 pb-4">
+        <table className="min-w-full border-collapse">
+          <thead className="sticky top-0 z-20 bg-[var(--itemsbackground)]">
+            <tr>
+              <th className="h-12 px-4 text-center text-xl font-light uppercase tracking-wider whitespace-nowrap sticky left-0 bg-[var(--itemsbackground)] w-10">
+                #
+              </th>
+              {headers.map((header, index) => (
+                <th
+                  key={index}
+                  className="h-12 px-4 text-center text-xl font-light uppercase tracking-wider whitespace-nowrap"
+                >
+                  {header}
+                </th>
+              ))}
+              <th className="h-12 px-4 text-center text-xl font-light uppercase tracking-wider whitespace-nowrap">
+                System
+              </th>
+            </tr>
+          </thead>
+          
+          <tbody>
+            {rows.map((row, rowIndex) => (
+              <tr
+                key={rowIndex}
+                className="hover:bg-[var(--itemsbackground)]/10 transition-colors"
+              >
+                <td className="px-4 text-center sticky left-0 bg-[var(--itemsbackground)] w-10">
+                  {rowIndex + 1}
+                </td>
+                {row.elements.map((value, colIndex) => (
+                  <td key={colIndex} className="px-4 py-2 text-center">
+                    <ToggleCell
+                      currentValue={value}
+                      onSelect={(val) => handleElementClick(rowIndex, colIndex, val)}
+                    />
+                  </td>
+                ))}
+                <td className="px-4 py-2 text-center">
+                  <ToggleCell
+                    currentValue={row.system}
+                    onSelect={(val) => handleSystemClick(rowIndex, val)}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      
+      <div className="flex items-center justify-center rounded-md transition-colors text-lg pt-5 space-x-5">
         <button className="w-20 h-10 bg-[var(--itemsbackground)] rounded-md" onClick={createFunction}>
           Create
+        </button>
+        <button className="w-20 h-10 bg-[var(--itemsbackground)] rounded-md" onClick={clearValues}>
+          Clear All
         </button>
       </div>
     </>
