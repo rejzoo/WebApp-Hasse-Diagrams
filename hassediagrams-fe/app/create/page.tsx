d100d844c4483b50ec3695d6ef14e7e2c81b1d7e
rejzoo <rejzho@gmail.com>
2025-02-28 12:28:13 +0100

    Added number of elements selector

--- hassediagrams-fe/app/create/page.tsx
+++ hassediagrams-fe/app/create/page.tsx
@@ -8,11 +8,20 @@ export default function CreatePage() {
     
     return (
         <div>
-            <input
-                type="number"
-                placeholder={`${numberOfElements}`}
-                onChange={(e) => setNumberOfElements(Number(e.target.value))}
-            />
+            <div className="flex flex-row items-center space-x-4 pb-8">
+                <span className="text-xl">Number of elements</span>
+                <select
+                    value={numberOfElements}
+                    onChange={(e) => setNumberOfElements(Number(e.target.value))}
+                    className="w-15 px-2 py-1 rounded-md focus:outline-none bg-[var(--itemsbackground)] text-[var(--foreground)] text-lg"
+                    >
+                    {Array.from({ length: 9 }, (_, i) => (
+                        <option key={i} value={i + 2}>
+                        {i + 2}
+                        </option>
+                    ))}
+                </select>
+            </div>
             <InputTable numberOfElements={numberOfElements}/>
         </div>
     );
