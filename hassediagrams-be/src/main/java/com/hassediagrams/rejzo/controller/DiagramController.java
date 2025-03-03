341b9a61edb80eea12c06d847499e7199a032ce2
rejzoo <rejzho@gmail.com>
2025-03-03 23:56:00 +0100

    Added API for fetching

--- hassediagrams-be/src/main/java/com/hassediagrams/rejzo/controller/DiagramController.java
+++ hassediagrams-be/src/main/java/com/hassediagrams/rejzo/controller/DiagramController.java
@@ -5,10 +5,7 @@ import com.hassediagrams.rejzo.dto.ElementDataWrapper;
 import com.hassediagrams.rejzo.service.DiagramService;
 import com.hassediagrams.rejzo.util.DiagramJSONGenerator;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/diagrams")
@@ -16,16 +13,37 @@ public class DiagramController {
 
     private final DiagramService diagramService;
 
+    // Just for testing for now
+    private DiagramData diagram;
+
     public DiagramController(DiagramService diagramService) {
         this.diagramService = diagramService;
     }
 
+    /**
+     * Creates the diagram
+     * TODO: inserts it into the table
+     *
+     * @param data input from user
+     * @return returns status
+     */
     @PostMapping("/create")
     public ResponseEntity<String> createDiagram(@RequestBody ElementDataWrapper data) {
-        DiagramData diagram = diagramService.createDiagram(data);
+        this.diagram = diagramService.createDiagram(data);
 
+        return ResponseEntity.ok("API Called, Result: ");
+    }
+
+    /**
+     * Fetches all diagrams from database
+     * TODO: database - for now just attributes
+     *
+     * @return returns diagram json data
+     */
+    @GetMapping("fetchAll")
+    public ResponseEntity<String> fetchDiagrams() {
         String json = DiagramJSONGenerator.generateDiagramJson(diagram);
 
-        return ResponseEntity.ok("API Called, Result: " + json);
+        return ResponseEntity.ok(json);
     }
 }
