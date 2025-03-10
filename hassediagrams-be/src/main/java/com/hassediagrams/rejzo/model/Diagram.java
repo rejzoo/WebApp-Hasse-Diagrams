f2ec904cbf56d091e830e4d66020f26189aefebf
rejzoo <rejzho@gmail.com>
2025-03-10 12:38:51 +0100

    Added JSONB conversion for collum

--- hassediagrams-be/src/main/java/com/hassediagrams/rejzo/model/Diagram.java
+++ hassediagrams-be/src/main/java/com/hassediagrams/rejzo/model/Diagram.java
@@ -1,9 +1,13 @@
 package com.hassediagrams.rejzo.model;
 
+import com.hassediagrams.rejzo.dto.DiagramData;
+import io.hypersistence.utils.hibernate.type.json.JsonBinaryType;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.Type;
+import org.hibernate.type.SqlTypes;
 
 @Entity
 @Table(name = "diagrams")
@@ -17,11 +21,14 @@ public class Diagram {
 
     private Integer user_id;
 
-    private String diagram;
+    @Type(JsonBinaryType.class)
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name = "diagram", columnDefinition = "jsonb")
+    private DiagramData diagram;
 
     private Diagram() {}
 
-    public Diagram(Integer user_id, String diagram) {
+    public Diagram(Integer user_id, DiagramData diagram) {
         this.user_id = user_id;
         this.diagram = diagram;
     }
