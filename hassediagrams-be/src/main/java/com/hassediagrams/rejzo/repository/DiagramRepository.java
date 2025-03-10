20ccb74a5fa316bef756e0ff331c4c8600239cda
rejzoo <rejzho@gmail.com>
2025-03-10 11:01:19 +0100

    Added Diagram repository

---
+++ hassediagrams-be/src/main/java/com/hassediagrams/rejzo/repository/DiagramRepository.java
@@ -0,0 +1,7 @@
+package com.hassediagrams.rejzo.repository;
+
+import com.hassediagrams.rejzo.model.Diagram;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DiagramRepository extends CrudRepository<Diagram, Integer> {
+}
